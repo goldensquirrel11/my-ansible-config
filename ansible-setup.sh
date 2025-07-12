1f602beb1e5abf644979d16ee0acae13a1206d5f
Sayid Abdur-Rahman Al-Aidarus <goldensquirrel11@gmail.com>
2025-07-12 22:29:44 +0800

    Fix ansible pull script

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -3,4 +3,6 @@
 # Install ansible
 sudo apt install ansible -y
 
-ansible-pull -U https://github.com/goldensquirrel11/my-ansible-config/ -u "$(id -un)" -K
+export PYTHONUNBUFFERED=1 # Disable buffering for Python output
+
+ansible-pull -U https://github.com/goldensquirrel11/my-ansible-config/ --full -u "$(id -un)" -K
