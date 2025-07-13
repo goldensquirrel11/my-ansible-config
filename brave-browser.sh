cc0e30649234b11dc3187dee0ebf2fa8d7af0b9b
Sayid Abdur-Rahman Al-Aidarus <goldensquirrel11@gmail.com>
2025-07-13 22:29:35 +0800

    Remove old software installation scripts

--- brave-browser.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Add Brave Browser repo & key to apt
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
